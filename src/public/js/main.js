f73a1adec09c2a639c64197832e8614c876c8722
leartt <leartqeku@gmail.com>
2021-10-29 22:53:45 +0200

    project

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,44 +1,113 @@
 const navbarToggler = document.querySelector('button.nav-toggle');
 const navLinks = document.querySelector('.nav__links');
 
-const careerForm = document.querySelector('.career form');
+const careerForm = document.getElementById('career-form');
+const contactForm = document.getElementById('contact-form');
 
 
-async function sendEmail(e) {
-    console.log(e);
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+function toastify(message, isSuccess) {
+    Toastify({
+        text: message,
+        duration: 5000,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        style: {
+            background: isSuccess ? "green" : "red"
+        }
+    }).showToast();
+}
+
+async function submitContactForm(e) {
     e.preventDefault();
 
-    const firstName = careerForm.first_name.value;
-    const lastName = careerForm.last_name.value;
-    const email = careerForm.email.value;
-    const phone = careerForm.phone.value;
-    const additionalInfo = careerForm.additional_info.value;
+    const name = escapeHtml(contactForm["name"].value);
+    const email = escapeHtml(contactForm["email"].value);
+    const message = escapeHtml(contactForm["message"].value);
 
-    const emailData = {
-        firstName,
-        lastName,
-        email,
-        phone,
-        additionalInfo
-    }
+    const formData = { name, email, message }
 
     const URL = `${location.protocol}//${location.host}`;
-    const response = await fetch(`${URL}/api/send-email`, {
+    const response = await fetch(`${URL}/api/contact-email`, {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify(emailData),
+        body: JSON.stringify(formData)
     })
 
     const data = await response.json();
-    console.log(data);
+    if (!data.success) {
+        return toastify(data.message, data.success)
+    }
+
+    toastify(data.message, data.success);
+    document.getElementById('contact-form').reset();
 
 }
 
-careerForm?.addEventListener('submit', sendEmail)
+async function submitCareerForm(e) {
+    e.preventDefault();
+
+    const firstName = escapeHtml(careerForm["firstName"].value);
+    const lastName = escapeHtml(careerForm["lastName"].value);
+    const email = escapeHtml(careerForm["email"].value);
+    const phone = escapeHtml(careerForm["phone"].value);
+    const additionalInfo = escapeHtml(careerForm["additionalInfo"].value);
+    const cvFile = careerForm["cvFile"].files[0];
+
+
+    const formData = new FormData();
+    formData.append("firstName", firstName)
+    formData.append("lastName", lastName)
+    formData.append("email", email)
+    formData.append("phone", phone)
+    formData.append("additionalInfo", additionalInfo)
+    formData.append("cvFile", cvFile)
+
+
+    formData.forEach(data => console.log(data));
+    // return;
+
+    const URL = `${location.protocol}//${location.host}`;
+    const response = await fetch(`${URL}/api/career-email`, {
+        method: "POST",
+        body: formData
+    })
+
+    const data = await response.json();
+    if (!data.success) {
+        return toastify(data.message, data.success)
+    }
+
+    toastify(data.message, data.success);
+    document.getElementById('career-form').reset();
+}
+
+contactForm?.addEventListener('submit', submitContactForm)
+careerForm?.addEventListener('submit', submitCareerForm)
 
 navbarToggler.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 })
 
+document.querySelectorAll('input').forEach(input => {
+    input.addEventListener('keyup', () => {
+        if (!input.checkValidity()) {
+            input.classList.add('is-invalid')
+        }
+        else {
+            input.classList.remove('is-invalid')
+            input.classList.add('is-valid')
+        }
+    })
+})
--- src/server.js
+++ src/server.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const path = require('path');
+const expressFileUpload = require('express-fileupload');
+const helmet = require('helmet');
+
 require('dotenv').config();
-const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }))
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressFileUpload({ limits: { fileSize: 1 * 1024 * 1024 } }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post("/api/send-email", async (req, res) => {
+app.post("/api/contact-email", async (req, res) => {
     try {
+        const { name, email, message } = req.body;
 
-        const { firstName, lastName, email, phone, additionalInfo } = req.body;
+        if (!email) return res.status(400).json({ success: false, message: "Please provide your email" });
 
-        console.log(req.body);
+        if (!message) return res.status(400).json({ success: false, message: "Please provide a message" });
 
-        console.log(process.env.NODE_EMAIL)
-        console.log(process.env.NODE_PASSWORD)
+        const plainTextMessage = `Name: ${name}, Email: ${email}, Message: ${message}`
+
+        const HTMLmessage = `
+            <div>
+            <p><strong>Name: </strong>${name}</p>
+            <p><strong>Email: </strong>${email}</p>
+            <p><strong>Message: </strong>${message}</p>
+            </div>
+        `
 
         const transporter = nodemailer.createTransport({
             host: "leadnationllc.com",
@@ -30,6 +44,43 @@ app.post("/api/send-email", async (req, res) => {
             },
         });
 
+        await transporter.sendMail({
+            from: email, // sender address
+            to: "info@leadnationllc.com", // list of receivers
+            subject: "New submission from Contact Form", // Subject line
+            text: plainTextMessage, // plain text body
+            html: HTMLmessage, // html body
+        });
+
+        return res.status(200).json({ success: true, message: "Email was sent successfully" });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: "Something went wrong. Plese try again" })
+    }
+})
+
+app.post("/api/career-email", async (req, res) => {
+    try {
+
+        const { firstName, lastName, email, phone, additionalInfo } = req.body;
+        const cvFile = req.files ? req.files.cvFile : undefined;
+
+        if (!firstName) {
+            return res.status(400).json({ success: false, message: "Please provide first name" });
+        }
+        if (!lastName) {
+            return res.status(400).json({ success: false, message: "Please provide last name" });
+        }
+        if (!email) {
+            return res.status(400).json({ success: false, message: "Please provide email" });
+        }
+        if (!cvFile) {
+            return res.status(400).json({ success: false, message: "Please provide your CV" });
+        }
+
+        const plainTextMessage = `First Name: ${firstName}, Last Name: ${lastName}, Email: ${email}, Phone: ${phone}, Additional Info: ${additionalInfo}`
+
         const HTMLmessage = `
             <div>
             <p><strong>First Name: </strong>${firstName}</p>
@@ -38,21 +89,37 @@ app.post("/api/send-email", async (req, res) => {
             <p><strong>Phone: </strong>${phone}</p>
             <p><strong>Additional Info: </strong>${additionalInfo}</p>
             </div>
-        `
+        `;
+
+        const transporter = nodemailer.createTransport({
+            host: "leadnationllc.com",
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user: process.env.NODE_EMAIL,
+                pass: process.env.NODE_PASSWORD,
+            },
+        });
 
         await transporter.sendMail({
             from: `${firstName} ${lastName} <${email}>`, // sender address
-            to: "info@leadnationllc.com", // list of receivers
-            subject: "New Email from Career Form", // Subject line
-            text: HTMLmessage, // plain text body
-            html: HTMLmessage // html body
+            to: "hr@leadnationllc.com", // list of receivers
+            subject: "New submission from Career Form", // Subject line
+            text: plainTextMessage, // plain text body
+            html: HTMLmessage, // html body
+            attachments: [
+                {
+                    filename: cvFile?.name,
+                    content: cvFile?.data,
+                }
+            ]
         });
 
         return res.status(200).json({ success: true, message: "Email was sent successfully" });
 
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ success: false, message: "Something went wrong with email" })
+        return res.status(500).json({ success: false, message: "Something went wrong. Please try again" })
     }
 });
 
